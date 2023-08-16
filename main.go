3a3cd1dbb9fd704b18b748eb894673c6c5b01b2a
Corné van Straten <corne.straten@array.com>
2023-08-16 14:09:58 -0400

    pull Eminem song (Not Afraid) and censor out a few bad words

--- main.go
+++ main.go
@@ -2,47 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
 
-type Quote struct {
-	Text string
-	Author string
-}
+func main() {
+	bad_words := []string{"fuck", "shit", "crap"}
 
-func main(){
-	quotes := []Quote{};
+	fmt.Println("Working")
 
 	c := colly.NewCollector(
-		colly.AllowedDomains("quotes.toscrape.com"),
+		colly.AllowedDomains("www.songlyrics.com", "songlyrics.com", "https://www.songlyrics.com"),
 	)
 
-	c.OnRequest(func(r *colly.Request){
-		r.Headers.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.OnResponse(func(r *colly.Response){
+	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Response Code", r.StatusCode)
 	})
 
-	c.OnError(func(r *colly.Response, err error){
+	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("error", err.Error())
 	})
 
-	c.OnHTML(".quote", func(h *colly.HTMLElement){
+	c.OnHTML("#songLyricsDiv-outer", func(h *colly.HTMLElement) {
 		div := h.DOM
-		quoteText := div.Find(".text").Text()
-		author := div.Find(".author").Text()
-		q := Quote{
-			Text: quoteText,
-			Author: author,
-		}
-		quotes = append(quotes, q)
+		lyrics := div.Find(".songLyricsV14").Text()
 
-		fmt.Println(quotes)
+		i := 0
+		for i < len(bad_words) {
+			lyrics = strings.Replace(lyrics, bad_words[i], "[****]", -1)
+			i += 1
+		}
+
+		fmt.Println(lyrics)
 	})
 
-	c.Visit("http://quotes.toscrape.com")
+	c.Visit("https://www.songlyrics.com/eminem/not-afraid-lyrics/")
 }
